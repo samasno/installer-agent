c6abe62e62818e328471d5c6d614c16fa461ac95
samasno <samasno@samasno.com>
2024-09-22 15:42:34 -0500

    chain middlewares, dependency injection

--- binary-server/main.go
+++ binary-server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/base64"
@@ -13,10 +12,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
 	"path/filepath"
 	"strings"
-	"syscall"
 	"time"
 )
 
@@ -60,68 +57,257 @@ func main() {
 		panic(err.Error())
 	}
 
-	srv := runServer(port)
+	var verifyOptions *x509.VerifyOptions
 
-	k := make(chan os.Signal, 5)
+	if CERT != "" {
+		opts := getVerifyOptions(CERT)
+		verifyOptions = &opts
+	}
 
-	signal.Notify(k, syscall.SIGTERM, syscall.SIGINT)
-
-	<-k
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(10)*time.Second)
-	defer cancel()
-
-	done := make(chan bool)
-
-	go func() {
-		err = srv.Shutdown(ctx)
-		if err != nil {
-			log.Fatal(err.Error())
+	srv := runServer(port, FILES_DIR, verifyOptions)
+	if err := srv.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			logger.Println("server closed")
+			os.Exit(0)
 		}
 
-		done <- true
-	}()
-
-	select {
-	case <-ctx.Done():
-		log.Println("failed to shutdown server in time")
+		logger.Println("server crashed")
 		os.Exit(1)
-	case <-done:
-		log.Println("server shutdown")
-		os.Exit(0)
 	}
 }
 
-func runServer(port int) *http.Server {
+func runServer(port int, filesDir string, verifyOptions *x509.VerifyOptions) *http.Server {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{OS}/{ARCH}/latest", logRequests(handleGetLatest))
-	mux.HandleFunc("PUT /{OS}/{ARCH}/latest/{VER}", requireCert(handleUpdateLatest))
-	mux.HandleFunc("GET /{OS}/{ARCH}/checksum/{VER}", logRequests(handleGetChecksum))
-	mux.HandleFunc("POST /{OS}/{ARCH}/upload", requireCert(logRequests(handleUploadBinary)))
-	mux.HandleFunc("GET /{OS}/{ARCH}/download/{VER}", logRequests(handleDownloadBinary))
-	mux.HandleFunc("GET /", logRequests(handleHome))
+	mux.HandleFunc("GET /{OS}/{ARCH}/latest", chainMiddleware(handleGetLatest(filesDir), logRequests))
+	mux.HandleFunc("PUT /{OS}/{ARCH}/latest/{VER}", chainMiddleware(handleUpdateLatest(filesDir), logRequests))
+	mux.HandleFunc("GET /{OS}/{ARCH}/checksum/{VER}", chainMiddleware(handleGetChecksum(filesDir), logRequests))
+	mux.HandleFunc("POST /{OS}/{ARCH}/upload", chainMiddleware(handleUploadBinary(filesDir), logRequests, requireCert(verifyOptions)))
+	mux.HandleFunc("GET /{OS}/{ARCH}/download/{VER}", chainMiddleware(handleDownloadBinary(filesDir), logRequests))
+	mux.HandleFunc("GET /{$}", chainMiddleware(handleHome, logRequests))
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf("0.0.0.0:%d", port),
 		Handler: mux,
 	}
 
-	go func(srv *http.Server) {
-		if err := srv.ListenAndServe(); err != nil {
-			if err == http.ErrServerClosed {
-				logger.Println("server shutdown gracefully")
-				return
-			}
-
-			logger.Println("server not shutdown gracefully")
-		}
-	}(srv)
-
 	return srv
 }
 
-func logRequests(h func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	replyMessage(w, http.StatusOK, "home page")
+}
+
+func handleGetLatest(files string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		OS := r.PathValue("OS")
+		ARCH := r.PathValue("ARCH")
+
+		p := filepath.Join(files, OS, ARCH, "latest")
+
+		b, err := os.ReadFile(p)
+		if err != nil {
+			replyMessage(w, http.StatusNotFound, "not found")
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
+	}
+}
+
+func handleUpdateLatest(files string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		OS := r.PathValue("OS")
+		ARCH := r.PathValue("ARCH")
+		VER := r.PathValue("VER")
+
+		dir := filepath.Join(files, OS, ARCH)
+
+		_, err := os.Stat(filepath.Join(dir, "bin", VER))
+		if err != nil {
+			if os.IsNotExist(err) {
+				replyMessage(w, http.StatusNotFound, "not found")
+				return
+			}
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		tmp, tmpPath, err := copyToTempFile(strings.NewReader(VER))
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		tmp.Close()
+
+		err = os.Rename(tmpPath, filepath.Join(dir, "latest"))
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		replyMessage(w, http.StatusOK, "OK")
+	}
+
+}
+
+func handleGetChecksum(files string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		OS := r.PathValue("OS")
+		VER := r.PathValue("VER")
+		ARCH := r.PathValue("ARCH")
+
+		p := filepath.Join(files, OS, ARCH, "checksum", VER)
+		b, err := os.ReadFile(p)
+		if err != nil {
+			replyMessage(w, http.StatusNotFound, "not found")
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
+	}
+}
+
+func handleDownloadBinary(files string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		OS := r.PathValue("OS")
+		ARCH := r.PathValue("ARCH")
+		VER := r.PathValue("VER")
+
+		binpath := filepath.Join(files, OS, ARCH, "bin", VER)
+
+		fileDownload(w, r, binpath)
+	}
+}
+
+func handleUploadBinary(files string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		defer r.Body.Close()
+		OS := r.PathValue("OS")
+		ARCH := r.PathValue("ARCH")
+
+		err := r.ParseMultipartForm(10 << 50)
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		checksum := r.FormValue("checksum")
+		version := r.FormValue("version")
+		isLatest := r.FormValue("latest")
+
+		_, err = os.Stat(filepath.Join(files, OS, ARCH, "bin", version))
+		if err != nil && !os.IsNotExist(err) {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		if err == nil {
+			replyMessage(w, http.StatusUnauthorized, "version already exists")
+			return
+		}
+
+		f, _, err := r.FormFile("binary")
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		if f == nil {
+			replyMessage(w, http.StatusBadRequest, "binary file required")
+			return
+		}
+
+		if version == "" || checksum == "" {
+			replyMessage(w, http.StatusBadRequest, "checksum and version name required")
+			return
+		}
+
+		tmpBinary, tmpBinaryPath, err := copyToTempFile(f)
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		chx, err := checksumString(tmpBinary)
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		tmpBinary.Close()
+
+		if chx != checksum {
+			replyMessage(w, http.StatusBadRequest, "file uploaded to server does not match provided checksum and file may be corrupted. try again")
+			return
+		}
+
+		cxf, tmpChecksumPath, err := copyToTempFile(strings.NewReader(checksum))
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		cxf.Close()
+
+		destDir := filepath.Join(files, OS, ARCH)
+
+		err = os.MkdirAll(filepath.Join(destDir, "bin"), 0777)
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		err = os.MkdirAll(filepath.Join(destDir, "checksum"), 0777)
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		destBinFile := filepath.Join(destDir, "bin", version)
+		destChecksum := filepath.Join(destDir, "checksum", version)
+		destLatest := filepath.Join(destDir, "latest")
+
+		err = os.Rename(tmpBinaryPath, destBinFile)
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		err = os.Rename(tmpChecksumPath, destChecksum)
+		if err != nil {
+			replyMessage(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		if isLatest == "true" {
+			rdr := strings.NewReader(version)
+			vf, tmpVerPath, err := copyToTempFile(rdr)
+			if err != nil {
+				replyMessage(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+			vf.Close()
+
+			err = os.Rename(tmpVerPath, destLatest)
+			if err != nil {
+				replyMessage(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+		}
+
+		replyMessage(w, http.StatusCreated, "file uploaded")
+	}
+}
+
+func logRequests(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		wh := newWrappedWriter(w)
 		h(wh, r)
@@ -158,8 +344,17 @@ func (wh *wrappedResponseWriter) Status() int {
 	return wh.statusCode
 }
 
-func requireCert(h func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
-	caPem, err := os.ReadFile(CERT)
+func chainMiddleware(f http.HandlerFunc, mws ...middleware) http.HandlerFunc {
+	for _, m := range mws {
+		f = m(f)
+	}
+	return f
+}
+
+type middleware func(http.HandlerFunc) http.HandlerFunc
+
+func getVerifyOptions(cafile string) x509.VerifyOptions {
+	caPem, err := os.ReadFile(cafile)
 	if err != nil {
 		logger.Println("failed to open certificate authority")
 		logger.Fatal(err.Error())
@@ -172,239 +367,50 @@ func requireCert(h func(http.ResponseWriter, *http.Request)) func(http.ResponseW
 	options := x509.VerifyOptions{
 		Roots: roots,
 	}
-	return func(w http.ResponseWriter, r *http.Request) {
-		p := r.Header.Get("X-Client-Certificate")
-		if p == "" {
-			replyMessage(w, http.StatusUnauthorized, "no valid certificate in header")
-			return
-		}
 
-		decoded, err := base64.StdEncoding.DecodeString(p)
-		if err != nil {
-			replyMessage(w, http.StatusBadRequest, "bad encoding")
-			return
-		}
-
-		block, _ := pem.Decode(decoded)
-		if block == nil {
-			replyMessage(w, http.StatusBadRequest, "no blocks decoded")
-			return
-		}
-
-		cert, err := x509.ParseCertificate(block.Bytes)
-		if err != nil {
-			replyMessage(w, http.StatusInternalServerError, "could not parse certificate")
-			return
-
-		}
-		_, err = cert.Verify(options)
-		if err != nil {
-			replyMessage(w, http.StatusUnauthorized, "this is not an authorized certificate")
-			return
-		}
-
-		h(w, r)
-	}
+	return options
 }
 
-func handleHome(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	replyMessage(w, http.StatusOK, "home page")
-}
+func requireCert(options *x509.VerifyOptions) middleware {
+	return func(h http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			if options == nil {
+				h(w, r)
+				return
+			}
+			p := r.Header.Get("X-Client-Certificate")
+			if p == "" {
+				replyMessage(w, http.StatusUnauthorized, "no valid certificate in header")
+				return
+			}
 
-func handleGetLatest(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	OS := r.PathValue("OS")
-	ARCH := r.PathValue("ARCH")
+			decoded, err := base64.StdEncoding.DecodeString(p)
+			if err != nil {
+				replyMessage(w, http.StatusBadRequest, "bad encoding")
+				return
+			}
 
-	p := filepath.Join(FILES_DIR, OS, ARCH, "latest")
+			block, _ := pem.Decode(decoded)
+			if block == nil {
+				replyMessage(w, http.StatusBadRequest, "no blocks decoded")
+				return
+			}
 
-	b, err := os.ReadFile(p)
-	if err != nil {
-		replyMessage(w, http.StatusNotFound, "not found")
-		return
-	}
+			cert, err := x509.ParseCertificate(block.Bytes)
+			if err != nil {
+				replyMessage(w, http.StatusInternalServerError, "could not parse certificate")
+				return
 
-	w.WriteHeader(http.StatusOK)
-	w.Write(b)
-}
+			}
+			_, err = cert.Verify(*options)
+			if err != nil {
+				replyMessage(w, http.StatusUnauthorized, "this is not an authorized certificate")
+				return
+			}
 
-func handleUpdateLatest(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	OS := r.PathValue("OS")
-	ARCH := r.PathValue("ARCH")
-	VER := r.PathValue("VER")
-
-	dir := filepath.Join(FILES_DIR, OS, ARCH)
-
-	_, err := os.Stat(filepath.Join(dir, "bin", VER))
-	if err != nil {
-		if os.IsNotExist(err) {
-			replyMessage(w, http.StatusNotFound, "not found")
-			return
-		}
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	tmp, tmpPath, err := copyToTempFile(strings.NewReader(VER))
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	defer tmp.Close()
-
-	err = os.Rename(tmpPath, filepath.Join(dir, "latest"))
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	replyMessage(w, http.StatusOK, "OK")
-}
-
-func handleGetChecksum(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	OS := r.PathValue("OS")
-	VER := r.PathValue("VER")
-	ARCH := r.PathValue("ARCH")
-
-	p := filepath.Join(FILES_DIR, OS, ARCH, "checksum", VER)
-	b, err := readFile(p)
-	if err != nil {
-		replyMessage(w, http.StatusNotFound, "not found")
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(b)
-}
-
-func handleDownloadBinary(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	OS := r.PathValue("OS")
-	ARCH := r.PathValue("ARCH")
-	VER := r.PathValue("VER")
-
-	binpath := filepath.Join(FILES_DIR, OS, ARCH, "bin", VER)
-
-	fileDownload(w, r, binpath)
-}
-
-func handleUploadBinary(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	OS := r.PathValue("OS")
-	ARCH := r.PathValue("ARCH")
-
-	err := r.ParseMultipartForm(10 << 50)
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	checksum := r.FormValue("checksum")
-	version := r.FormValue("version")
-	isLatest := r.FormValue("latest")
-
-	_, err = os.Stat(filepath.Join(FILES_DIR, OS, ARCH, "bin", version))
-	if err != nil && !os.IsNotExist(err) {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	if err == nil {
-		replyMessage(w, http.StatusUnauthorized, "version already exists")
-		return
-	}
-
-	f, _, err := r.FormFile("binary")
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	if f == nil {
-		replyMessage(w, http.StatusBadRequest, "binary file required")
-		return
-	}
-
-	if version == "" || checksum == "" {
-		replyMessage(w, http.StatusBadRequest, "checksum and version name required")
-		return
-	}
-
-	tmpBinary, tmpBinaryPath, err := copyToTempFile(f)
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	defer tmpBinary.Close()
-
-	chx, err := checksumString(tmpBinary)
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	if chx != checksum {
-		replyMessage(w, http.StatusBadRequest, "file uploaded to server does not match provided checksum and file may be corrupted. try again")
-		return
-	}
-
-	cxf, tmpChecksumPath, err := copyToTempFile(strings.NewReader(checksum))
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	defer cxf.Close()
-
-	destDir := filepath.Join(FILES_DIR, OS, ARCH)
-
-	err = os.MkdirAll(filepath.Join(destDir, "bin"), 0777)
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	err = os.MkdirAll(filepath.Join(destDir, "checksum"), 0777)
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	destBinFile := filepath.Join(destDir, "bin", version)
-	destChecksum := filepath.Join(destDir, "checksum", version)
-	destLatest := filepath.Join(destDir, "latest")
-
-	err = os.Rename(tmpBinaryPath, destBinFile)
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	err = os.Rename(tmpChecksumPath, destChecksum)
-	if err != nil {
-		replyMessage(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	if isLatest == "true" {
-		rdr := strings.NewReader(version)
-		vf, tmpVerPath, err := copyToTempFile(rdr)
-		if err != nil {
-			replyMessage(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-		defer vf.Close()
-
-		err = os.Rename(tmpVerPath, destLatest)
-		if err != nil {
-			replyMessage(w, http.StatusInternalServerError, err.Error())
-			return
+			h(w, r)
 		}
 	}
-
-	replyMessage(w, http.StatusCreated, "file uploaded")
 }
 
 func copyToTempFile(src io.Reader) (*os.File, string, error) {
@@ -455,25 +461,4 @@ func fileDownload(w http.ResponseWriter, r *http.Request, path string) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 
 	http.ServeContent(w, r, fileName, time.Now(), f)
-}
-
-func readFile(path string) ([]byte, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	info, err := f.Stat()
-	if err != nil {
-		return nil, err
-	}
-
-	b := make([]byte, info.Size())
-	_, err = f.Read(b)
-	if err != nil {
-		return nil, err
-	}
-
-	return b, nil
 }
